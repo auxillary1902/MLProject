b8141a7e02a1f7c7fb346c86204eb74d565c6da7
Sudarshan Parthasarathy <omegacore123@yahoo.in>
2019-04-27 16:23:40 +0530

    add projectreport

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ import pandas
 from sklearn import model_selection
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
-#from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB
 
 dataframe = pandas.read_csv("satisfactionmain.csv")
 array = dataframe.values
@@ -20,10 +20,10 @@ seed = 7
 
 kfold = model_selection.KFold(n_splits=10, random_state=seed)
 cart = DecisionTreeClassifier()
-#gnb = GaussianNB()
+gnb = GaussianNB()
 
 num_trees = 6
-model = BaggingClassifier(base_estimator=cart, n_estimators=num_trees, random_state=seed)
+model = BaggingClassifier(base_estimator=gnb, n_estimators=num_trees, random_state=seed)
 results = model_selection.cross_val_score(model, X, Y, cv=kfold)
 print("Performing bagging with 10-fold cross-validation. The accuracy for each of the 10 folds are:")
 print(results)
